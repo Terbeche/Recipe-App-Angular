81703704dfbf52bf57f968f50a616dbd499f81fd
mostefa <termus96@gmail.com>
2023-04-22 12:56:29 +0100

    Add navbar and basic views

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from "@angular/core";
     templateUrl: "./header.component.html"
 })
 export class HeaderComponent implements OnInit {
-
+  collapsed = true;
   ngOnInit() {}
 
 }
