40bf17c536e2c53db5172553ab1ca284dbb8270b
mostefa <termus96@gmail.com>
2023-04-28 12:39:51 +0100

    Add navigation and for loop for recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector: "app-header",
@@ -7,5 +7,10 @@ import { Component, OnInit } from "@angular/core";
 export class HeaderComponent implements OnInit {
   collapsed = true;
   ngOnInit() {}
+  @Output() featureSelected = new EventEmitter<string>();
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
+
 
 }
