c8d2104e592d5b37df69ebef7d0766000c434bf4
mostefa <termus96@gmail.com>
2023-04-28 23:49:52 +0100

    Refactor dropdown

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,19 +1,20 @@
-import { Directive, HostBinding, HostListener, OnInit, OnDestroy } from '@angular/core';
-
+import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
+ 
 @Directive({
     selector: '[appDropdown]'
 })
 
-export class DropdownDirective implements OnInit, OnDestroy {
+export class DropdownDirective{
 
     @HostBinding('class.open') isOpen = false;
-    @HostListener('click') toggleOpen() {
-        this.isOpen = !this.isOpen;
-    }
-    constructor() { }
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+      }
+    constructor(private elRef: ElementRef) {}
     
-    ngOnInit() {
-    }
-    ngOnDestroy() {
-    }
 }
+
+
+
+
+
